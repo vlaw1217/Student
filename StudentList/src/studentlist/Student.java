1f47068267948c3206fce8c74a83a4505ec7d8a9
Vicky Law <lawye@sheridancollege.ca>
2020-05-31 12:23:27 -0400

    First commit - PartTime

--- StudentList/src/studentlist/Student.java
+++ StudentList/src/studentlist/Student.java
@@ -8,6 +8,26 @@ package studentlist;
 public class Student {
     
     private String name;
+    private String numHours;
+
+    /**
+     * Get the value of numHours
+     *
+     * @return the value of numHours
+     */
+    public String getNumHours() {
+        return numHours;
+    }
+
+    /**
+     * Set the value of numHours
+     *
+     * @param numHours new value of numHours
+     */
+    public void setNumHours(String numHours) {
+        this.numHours = numHours;
+    }
+
 
     public Student(String name) {
         this.name = name;
